def12757989c7d326b49759c999f07ea5cc8cadc
Divya Dharshini A <31720620+addharshini@users.noreply.github.com>
2026-02-21 12:37:37 -0800

    Add 2163_kth_distinct_string_in_an_array.py - LeetCode Problem 2163

---
+++ 2163_kth_distinct_string_in_an_array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        a=[]
+        for i in arr:
+            if arr.count(i)==1:
+                a.append(i)
+        print(a)
+        if len(a)<k:
+            return ""
+        else:
+            return a[k-1]
